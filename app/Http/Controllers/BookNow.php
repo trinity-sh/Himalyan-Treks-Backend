37bc475bffbc510b18a3f43d279dcd533679b967
Soumali Gorai <gorai.soumali@gmail.com>
2022-06-25 12:12:59 +0530

    thora kam kiye

--- app/Http/Controllers/BookNow.php
+++ app/Http/Controllers/BookNow.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BookNowModel;
 
 class BookNow extends Controller
 {
     //
-    function bookNow(){
-        return("hello world");
+    function bookNow(Request $req){
+        return($req->input());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("booknow",[BookNow::class,"bookNow"]);
+Route::post('bookNow',[BookNow::class,"bookNow"]);
