76d6031e103acd9bc660d700581396bf971b2349
Debdeep1 <deb.baner.1@gmail.com>
2022-07-06 16:25:46 +0530

    ...

--- app/Http/Controllers/DiscountTreksController.php
+++ app/Http/Controllers/DiscountTreksController.php
@@ -15,19 +15,19 @@ class DiscountTreksController extends Controller
         $days = $req->input('days');
         $price = $req->input('price');
         // file input
-        $img = $req->file('img')->store("treksImages");
+        $img = $req->input('img');
         $img_desp = $req->input('img_desp');
         $location = $req->input('location');
         $camp_location = $req->input('camp_location');
         $height = $req->input('height');
         $desp = $req->input('desp');
         $iternery = $req->input('iternery');
-        $gallery_img1 = $req->file('gallery_img1')->store("treksImages/gallery");
-        $gallery_img2 = $req->file('gallery_img2')->store("treksImages/gallery");
-        $gallery_img3 = $req->file('gallery_img3')->store("treksImages/gallery");
-        $gallery_img4 = $req->file('gallery_img4')->store("treksImages/gallery");
-        $gallery_img5 = $req->file('gallery_img5')->store("treksImages/gallery");
-        $gallery_img6 = $req->file('gallery_img6')->store("treksImages/gallery");
+        $gallery_img1 = $req->input('gallery_img1');
+        $gallery_img2 = $req->input('gallery_img2');
+        $gallery_img3 = $req->input('gallery_img3');
+        $gallery_img4 = $req->input('gallery_img4');
+        $gallery_img5 = $req->input('gallery_img5');
+        $gallery_img6 = $req->input('gallery_img6');
         
         $trek = array(
             'title' => $title, 'days' => $days, 'price' => $price, 'img' => $img, 'img_desp' => $img_desp, 'title' => $title, 'location' => $location, 'camp_location' => $camp_location, 'height' => $height, 'desp' => $desp, 'iternery' => $iternery, 'gallery_img1' => $gallery_img1, 'gallery_img2' => $gallery_img2, 'gallery_img3' => $gallery_img3,'gallery_img4' => $gallery_img4, 'gallery_img5' => $gallery_img5, 'gallery_img6' => $gallery_img6,
