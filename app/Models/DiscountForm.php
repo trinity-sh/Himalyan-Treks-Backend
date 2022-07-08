5d92aadcdbee18ebf8a417325102ffd2bc937ed3
Soumali Gorai <gorai.soumali@gmail.com>
2022-07-08 15:10:34 +0530

    discount route

--- app/Models/DiscountForm.php
+++ app/Models/DiscountForm.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class DiscountForm extends Model
 {
     use HasFactory;
+    public $table = "discountforms";
+    protected $fillable = [
+        'title',
+        'days',
+        'price',
+        'img',
+        'img_desp',
+        'location',
+        'camp_location',
+        'height',
+        'desp',
+        'iternery',
+        'gallery_img1',
+        'gallery_img2',
+        'gallery_img3',
+        'gallery_img4',
+        'gallery_img5',
+        'gallery_img6',
+
+    ];
+    public $timestamps=false;
 }
